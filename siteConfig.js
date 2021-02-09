f09399b3485ce1514f316b2294c303c641bd2cfe
coolboyhy1607 <coolboyhy1607@gmail.com>
2021-02-09 22:03:21 +0900

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://seren.ooo`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
